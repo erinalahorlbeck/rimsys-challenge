7bef37905c03d7af6be4c848caf32f0d240a2d62
Erinala Horlbeck <imn.exc@gmail.com>
2022-11-02 22:24:03 +0000

    Add api routes for users, documents

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,13 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\Api\V1\AuthApiController;
+use App\Http\Controllers\Api\V1\UserApiController;
+use App\Http\Controllers\Api\V1\DocumentApiController;
+
+use App\Models\User;
+use App\Models\Document;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +21,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+
+Route::prefix('v1')->group(function() {
+    Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+    // Route::post('/login', [AuthApiController::class, 'login']);
+
+    Route::resource('users', UserApiController::class);
+
+    Route::resource('documents', DocumentApiController::class);
+
+    Route::get('/users/{user}/documents/{document}', function(User $user, Document $document) {
+        return $document;
+    })->scopeBindings();
 });
